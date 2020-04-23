7ad327bd93949011f63f55fdd86f842db2c5a6e5
deepak2703android <58721956+deepak2703android@users.noreply.github.com>
2020-04-23 21:49:28 +0530

    Code Is converted to Android Oreo and above

--- app/src/main/java/com/example/shortcutapp/MainActivity.java
+++ app/src/main/java/com/example/shortcutapp/MainActivity.java
@@ -1,59 +1,100 @@
 package com.example.shortcutapp;
 
+
+import android.accessibilityservice.AccessibilityService;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button shortcut ,secondactivity;
+    Button shortcut;
+    private Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        shortcut=findViewById(R.id.shortcut);
-        secondactivity=findViewById(R.id.activity2);
+        shortcut = findViewById(R.id.shortcut);
 
 
         shortcut.setOnClickListener(new View.OnClickListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
-                createShortcutOfApp();
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) ;
+                {
+
+                    ShortcutManager shortcutManager = (ShortcutManager) getSystemService(Context.SHORTCUT_SERVICE);
+
+                    if (shortcutManager.isRequestPinShortcutSupported()) {
+
+                        ShortcutInfo pinned = new ShortcutInfo.Builder(MainActivity.this,"Pinned")
+                                .setShortLabel("pin")
+                                .setLongLabel("pinned")
+                                .setIcon(Icon.createWithResource(MainActivity.this, R.drawable.shortcut))
+                                .setIntent(new Intent(MainActivity.this, Shortcut.class).setAction("pinned"))
+                                .build();
+
+                        if (alreadyexist(pinned)) {
+                            Toast.makeText(MainActivity.this, "Already exist", Toast.LENGTH_SHORT).show();
+                        }
+
+                        shortcutManager.requestPinShortcut(pinned, null);
+
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "Shortcut Not Supported On thos device", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+
+
             }
         });
-        secondactivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent second = new Intent(getApplicationContext(),
-                        Shortcut.class);
-                startActivity(second);
-            }
-        });
-
     }
 
-    private void createShortcutOfApp() {
+    @RequiresApi(api = Build.VERSION_CODES.N_MR1)
+    private boolean alreadyexist(ShortcutInfo shortcut) {
 
-        Intent shortcutIntent = new Intent(getApplicationContext(),
-                Shortcut.class);
-        shortcutIntent.setAction(Intent.ACTION_MAIN);
+        boolean exist = false;
 
-        Intent addIntent = new Intent();
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "2nd Activity");
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
-                Intent.ShortcutIconResource.fromContext(getApplicationContext(),
-                      R.drawable.shortcut));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) ;
+        {
 
-        addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-        addIntent.putExtra("duplicate", true);  //may it's already there so   don't duplicate
-        getApplicationContext().sendBroadcast(addIntent);
+            ShortcutManager shortcutManager = (ShortcutManager) getSystemService(Context.SHORTCUT_SERVICE);
 
+            for (int i = 0; i < shortcutManager.getPinnedShortcuts().size(); i++) {
 
+                ShortcutInfo shortcutInfo = shortcutManager.getPinnedShortcuts().get(i);
+
+                if (shortcut.getId().equals(shortcutInfo.getId())) {
+                    exist = true;
+                    break;
+                }
+
+            }} return exist;
     }
+
 }
